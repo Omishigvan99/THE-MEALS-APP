68371afaf2522d29e97a5dbeb584ab6ffb658e3f
Omkar Shigvan <omishigvan99@gmail.com>
2022-02-22 16:35:35 +0530

    Added Tab Navigtor to App

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MealCategoriesScreen from "../screens/MealCategoriesScreen";
 import CategoryScreen from "../screens/CategoryScreen";
 import MealDetialsScreen from "../screens/MealDetailsScreen";
+import FavouriteMealsScreen from "../screens/FavouriteMealsScreen";
 import { colors } from "../constants/colors";
 import { Platform, Image } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../components/CustomHeaderButton";
+import { MaterialIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
 // logo component to add to screen header
 // function LogoTitle() {
 //     return (
@@ -20,64 +26,110 @@ const Stack = createNativeStackNavigator();
 //     );
 // }
 
+let StackNavigator = () => (
+    <Stack.Navigator
+        screenOptions={{
+            headerStyle: {
+                backgroundColor:
+                    Platform.OS === "android" ? colors.primaryColor : "#FFF",
+            },
+            headerTintColor:
+                Platform.OS === "android" ? "#FFF" : colors.primaryColor,
+            headerTitleStyle: {
+                fontFamily: "pacifico",
+            },
+        }}
+    >
+        <Stack.Screen
+            name="Meal Categories"
+            component={MealCategoriesScreen}
+        ></Stack.Screen>
+        <Stack.Screen
+            name="Category Meals"
+            component={CategoryScreen}
+            options={({ route }) => ({
+                title: route.params.categoryName,
+            })}
+        ></Stack.Screen>
+        <Stack.Screen
+            name="Meal Details"
+            component={MealDetialsScreen}
+            // you could also add icon as header
+            // options={{
+            //     headerTitle: (props) => (
+            //         <LogoTitle {...props}></LogoTitle>
+            //     ),
+            // }}
+            options={({ route }) => ({
+                title: route.params.mealTitle,
+                headerRight: () => (
+                    <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+                        <Item
+                            title="fav"
+                            iconName="favorite"
+                            onPress={() => {
+                                console.log("Favorite got clicked");
+                            }}
+                        ></Item>
+                    </HeaderButtons>
+                ),
+            })}
+        ></Stack.Screen>
+    </Stack.Navigator>
+);
+
 const AppNavigator = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator
+            <Tab.Navigator
                 screenOptions={{
-                    headerStyle: {
-                        backgroundColor:
-                            Platform.OS === "android"
-                                ? colors.primaryColor
-                                : "#FFF",
-                    },
-                    headerTintColor:
-                        Platform.OS === "android"
-                            ? "#FFF"
-                            : colors.primaryColor,
-                    headerTitleStyle: {
-                        fontFamily: "pacifico",
+                    headerShown: false,
+                    tabBarActiveTintColor: colors.primaryColor,
+                    tabBarLabelStyle: {
+                        fontFamily: "open-sans-regular",
+                        marginBottom: 3,
+                        fontSize: 12,
                     },
                 }}
             >
-                <Stack.Screen
-                    name="Meal Categories"
-                    component={MealCategoriesScreen}
-                ></Stack.Screen>
-                <Stack.Screen
-                    name="Category Meals"
-                    component={CategoryScreen}
-                    options={({ route }) => ({
-                        title: route.params.categoryName,
-                    })}
-                ></Stack.Screen>
-                <Stack.Screen
-                    name="Meal Details"
-                    component={MealDetialsScreen}
-                    // you could also add icon as header
-                    // options={{
-                    //     headerTitle: (props) => (
-                    //         <LogoTitle {...props}></LogoTitle>
-                    //     ),
-                    // }}
-                    options={({ route }) => ({
-                        title: route.params.mealTitle,
-                        headerRight: () => (
-                            <HeaderButtons
-                                HeaderButtonComponent={CustomHeaderButton}
-                            >
-                                <Item
-                                    title="fav"
-                                    iconName="favorite"
-                                    onPress={() => {
-                                        console.log("Favorite got clicked");
-                                    }}
-                                ></Item>
-                            </HeaderButtons>
+                <Tab.Screen
+                    name="Meals"
+                    component={StackNavigator}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialIcons
+                                name="restaurant"
+                                size={size}
+                                color={color}
+                            />
                         ),
-                    })}
-                ></Stack.Screen>
-            </Stack.Navigator>
+                    }}
+                ></Tab.Screen>
+                <Tab.Screen
+                    name="Favorites"
+                    component={FavouriteMealsScreen}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialIcons
+                                name="favorite"
+                                size={size}
+                                color={color}
+                            ></MaterialIcons>
+                        ),
+                        headerShown: true,
+                        headerStyle: {
+                            backgroundColor: colors.primaryColor,
+                        },
+                        headerTintColor:
+                            Platform.OS === "ios"
+                                ? colors.primaryColor
+                                : "#fff",
+                        headerTitleStyle: {
+                            fontFamily: "pacifico",
+                        },
+                    }}
+                ></Tab.Screen>
+            </Tab.Navigator>
         </NavigationContainer>
     );
 };
